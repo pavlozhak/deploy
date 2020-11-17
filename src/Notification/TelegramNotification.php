3a2988469a6c8723b558665580655de214db038f
pavlo-zhak <zhak.pavlo@tftc.dev>
2020-11-17 23:45:20 +0200

    Test deploy build

--- src/Notification/TelegramNotification.php
+++ src/Notification/TelegramNotification.php
@@ -14,27 +14,27 @@ use TgBotApi\BotApiBase\Method\SendMessageMethod;
 class TelegramNotification {
 
     public static function prepare_message(stdClass $message) {
-        $notify_header = '\xF0\x9F\x9A\x80 <b>St-Dev Deploy</b>' . PHP_EOL;
+        $notify_header = "U+1F680" . " <b>#St-Dev Deploy</b>" . PHP_EOL . PHP_EOL;
         $notify_footer = "<b>Deploy end</b>";
 
         $notify_body = 'The Github user <a href="https://github.com/'
                        . $message->payload->pusher->name .'">@' . $message->payload->pusher->name . '</a>'
-                       . ' has pushed to ' . $message->payload->repository->url . PHP_EOL;
+                       . ' has pushed to ' . $message->payload->repository->url . PHP_EOL . PHP_EOL;
 
-        $notify_body .= 'Here\'s a brief list of what has been changed:' . PHP_EOL;
+        $notify_body .= 'Here\'s a brief list of what has been changed:' . PHP_EOL . PHP_EOL;
 
 
         foreach ($message->payload->commits as $commit) {
-            $notify_body .= '\xE2\x9C\x85 ' . $commit->message . PHP_EOL;
+            $notify_body .= "U+2705" . $commit->message . PHP_EOL;
             $notify_body .= 'Added: <i>' . count($commit->added) . PHP_EOL
                 .'</i>modified: <i>' . count($commit->modified) . PHP_EOL
                 .'</i>removed: <i>'.count($commit->removed) . PHP_EOL
                 .'</i><a href="' . $commit->url
-                . '">read more</a>' . PHP_EOL;
+                . '">read more</a>' . PHP_EOL . PHP_EOL;
         }
 
         $notify_body .= '<b>What follows is the output of the script:</b>' . PHP_EOL;
-        $notify_body .= '<pre>' . $message->output . '</pre>' . PHP_EOL;
+        $notify_body .= '<pre>' . $message->output . '</pre>' . PHP_EOL . PHP_EOL;
 
         return $notify_header . $notify_body . $notify_footer;
     }
