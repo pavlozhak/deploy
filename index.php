65565b8b5e99f9e9d85b3f0caed221792eff9318
pavlo-zhak <zhak.pavlo@tftc.dev>
2020-11-17 22:58:48 +0200

    Test deploy build

--- index.php
+++ index.php
@@ -4,6 +4,11 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once 'src/Deploy.php';
+require_once 'src/Config/Config.php';
+require_once 'src/Exception/DeployException.php';
+require_once 'src/Handlers/Git.php';
+require_once 'src/Notification/TelegramNotification.php';
 require_once 'vendor/autoload.php';
 
 use Deploy\Deploy;
