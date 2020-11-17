ebd8222647e7394a03e00bec14d9c111a18537e0
pavlo-zhak <zhak.pavlo@tftc.dev>
2020-11-18 00:12:11 +0200

    Test deploy build

--- src/Handlers/Git.php
+++ src/Handlers/Git.php
@@ -31,7 +31,6 @@ class Git {
     }
 
     public function update() {
-        var_dump($this->payload);
         foreach ($this->config->git->endpoints as $endpoint) {
             if($this->payload->repository->owner->html_url == self::GIT_URL . $endpoint->repo && $this->payload->ref == self::GIT_REFS . $endpoint->branch) {
                 ob_start();
